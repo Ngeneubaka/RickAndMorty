87c3765d23c847ece527f132236bf8dad86c0caf
ubaka <ngeneubaka1@gmail.com>
2023-05-02 13:05:53 +0100

    Created Rick and Morty project

---
+++ RickAndMorty/Models/RMCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacter.swift
+//  RickAndMorty
+//
+//  Created by Ubaka Ngene on 01/05/2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  RickAndMorty
+//
+//  Created by Ubaka Ngene on 01/05/2023.
+//
+
+import Foundation
