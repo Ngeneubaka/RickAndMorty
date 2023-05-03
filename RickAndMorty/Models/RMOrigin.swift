95b7a5daeb1bc12ebf15e1311947ca6af6ad356c
ubaka <ngeneubaka1@gmail.com>
2023-05-03 12:34:44 +0100

    Added Models

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,13 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Ubaka Ngene on 03/05/2023.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    let name: String
+    let url: String
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Ubaka Ngene on 03/05/2023.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    let name: String
+    let url: String
+}
